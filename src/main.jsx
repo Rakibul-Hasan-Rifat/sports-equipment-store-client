b66fe2dd1c59199ff61662c346fd0fb91ee0cdaf
Rakibul Hasan <rakibulhasan745101@gmail.com>
2025-03-07 23:47:22 +0600

    protected route is added to the private components

--- src/main.jsx
+++ src/main.jsx
@@ -8,8 +8,9 @@ import Register from "./components/Register.jsx";
 import HomeLayout from "./layouts/HomeLayout.jsx";
 import AddEquipment from "./pages/AddEquipment.jsx";
 import MyEquipments from "./pages/MyEquipments.jsx";
-import AuthProvider from "./providers/AuthProvider.jsx";
 import DataDetails from "./components/DataDetails.jsx";
+import AuthProvider from "./providers/AuthProvider.jsx";
+import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import SportEquipmentsPage from "./pages/SportEquipmentsPage.jsx";
 import TabularDataSection from "./components/TabularDataSection.jsx";
 
@@ -22,10 +23,31 @@ createRoot(document.getElementById("root")).render(
             <Route index element={<HomePage />} />
             <Route path="equipments" element={<SportEquipmentsPage />}>
               <Route index element={<TabularDataSection />} />
-              <Route path=":id" element={<DataDetails />} />
+              <Route
+                path=":id"
+                element={
+                  <ProtectedRoute>
+                    <DataDetails />
+                  </ProtectedRoute>
+                }
+              />
             </Route>
-            <Route path="addEquipment" element={<AddEquipment />} />
-            <Route path="myEquipments" element={<MyEquipments />} />
+            <Route
+              path="addEquipment"
+              element={
+                <ProtectedRoute>
+                  <AddEquipment />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="myEquipments"
+              element={
+                <ProtectedRoute>
+                  <MyEquipments />
+                </ProtectedRoute>
+              }
+            />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
           </Route>
---
+++ src/routes/ProtectedRoute.jsx
@@ -0,0 +1,20 @@
+import { useContext } from "react";
+import { Navigate } from "react-router";
+import { AuthContext } from "../providers/AuthProvider";
+
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading)
+    return (
+      <div className="flex items-center justify-center my-8">
+        <span className="loading loading-infinity loading-xl w-[50px]"></span>
+      </div>
+    );
+
+  if (!user) return <Navigate to={"/login"} />;
+
+  return children;
+};
+
+export default ProtectedRoute;
