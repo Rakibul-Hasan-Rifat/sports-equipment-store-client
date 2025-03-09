56db3b68cf02c0ea58b3d80a808d6f0bef9403be
Rakibul Hasan <rakibulhasan745101@gmail.com>
2025-03-09 23:05:13 +0600

    card delete functionality is added in the my-equpiment-page

--- src/pages/MyEquipments.jsx
+++ src/pages/MyEquipments.jsx
@@ -1,18 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../providers/AuthProvider";
+import toast, { Toaster } from "react-hot-toast";
 
 const MyEquipments = () => {
   const [productsByEmail, setProductsByEmail] = useState([]);
   const { user } = useContext(AuthContext);
   console.log(productsByEmail);
-
-  const handleUpdate = (id) => {
-    console.log("update btn clicked", id);
-  };
-
-  const handleDelete = (id) => {
-    console.log("delete btn clicked", id);
-  };
 
   useEffect(() => {
     fetch(
@@ -23,43 +16,62 @@ const MyEquipments = () => {
       .catch((err) => console.log(err));
   }, [user.email]);
 
+  const handleUpdate = (id) => {
+    console.log("update btn clicked", id);
+  };
+
+  const handleDelete = (id) => {
+    console.log("delete btn clicked", id);
+    fetch(`https://sport-equipment-store-server.vercel.app/products/${id}`)
+      .then((res) => res.json())
+      .then((result) => {
+        if (result.deletedCount) {
+          toast.success(`The item with id ${id} is deleted successfully.`);
+          setProductsByEmail(productsByEmail.filter((prd) => prd._id !== id));
+        }
+      });
+  };
+
   return (
-    <div className="my-8 grid grid-cols-3 gap-3">
-      {productsByEmail?.map((product) => (
-        <div className="card bg-base-100 w-96 shadow-sm">
-          <figure>
-            <img
-              src={product?.image_url}
-              alt="product"
-              className="aspect-3/2 object-cover"
-            />
-          </figure>
-          <div className="card-body">
-            <h2 className="card-title">
-              {product?.item_name}
-              <div className="badge badge-secondary text-white">
-                $ {product?.price}
+    <>
+      <Toaster />
+      <div className="my-8 grid grid-cols-3 gap-3">
+        {productsByEmail?.map((product) => (
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img
+                src={product?.image_url}
+                alt="product"
+                className="aspect-3/2 object-cover"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">
+                {product?.item_name}
+                <div className="badge badge-secondary text-white">
+                  $ {product?.price}
+                </div>
+              </h2>
+              <p>{product?.description}</p>
+              <div className="card-actions justify-end">
+                <button
+                  className="btn"
+                  onClick={() => handleUpdate(product?._id)}
+                >
+                  Update
+                </button>
+                <button
+                  className="btn"
+                  onClick={() => handleDelete(product?._id)}
+                >
+                  Delete
+                </button>
               </div>
-            </h2>
-            <p>{product?.description}</p>
-            <div className="card-actions justify-end">
-              <button
-                className="btn"
-                onClick={() => handleUpdate(product?._id)}
-              >
-                Update
-              </button>
-              <button
-                className="btn"
-                onClick={() => handleDelete(product?._id)}
-              >
-                Delete
-              </button>
             </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 };
 
